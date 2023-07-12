72725cd4b5b61e032d2d90c648cfd13c58fb40de
James Cocker <purplespider@users.noreply.github.com>
2023-07-12 16:55:39 +0100

    Fix CustomLumberjack Tab insertion

--- code/CustomLumberjack.php
+++ code/CustomLumberjack.php
@@ -9,17 +9,19 @@ use SilverStripe\Forms\Tab;
 use SilverStripe\Dev\Debug;
 use SilverStripe\CMS\Model\SiteTree;
 
-class CustomLumberjack extends Lumberjack {
+class CustomLumberjack extends Lumberjack
+{
 
-  /**
+	/**
 	 * This is responsible for adding the child pages tab and gridfield.
-   * CUSTOM: Customised to make the GridField tab first.
+	 * CUSTOM: Customised to make the GridField tab first.
 	 *
 	 * @param FieldList $fields
 	 */
-	public function updateCMSFields(FieldList $fields) {
+	public function updateCMSFields(FieldList $fields)
+	{
 		$excluded = $this->owner->getExcludedSiteTreeClassNames();
-		if(!empty($excluded)) {
+		if (!empty($excluded)) {
 			$pages = $this->getLumberjackPagesForGridfield($excluded);
 			$gridField = new GridField(
 				"ChildPages",
@@ -29,19 +31,18 @@ class CustomLumberjack extends Lumberjack {
 			);
 
 			$tab = new Tab('ChildPages', $this->getLumberjackTitle(), $gridField);
-			
+
 			// BEGIN CUSTOMISATION
-			
+
 			// $fields->insertAfter($tab, 'Main');
-			
-			if (SiteTree::get()->filter('ParentID',$this->owner->ID)->count()){
-				$fields->insertBefore($tab, 'Main'); 
+
+			if (SiteTree::get()->filter('ParentID', $this->owner->ID)->count()) {
+				$fields->insertBefore('Main', $tab);
 			} else {
-				$fields->insertAfter($tab, 'Main'); 
+				$fields->insertAfter('Main', $tab);
 			}
 			// END CUSTOMISATION
-			
+
 		}
 	}
-  
 }
